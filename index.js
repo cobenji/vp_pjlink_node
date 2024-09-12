8b6bb99994d9ffc27a34c5a0f4393108410c1ce0
Cobenji <>
2024-09-12 23:55:36 +0200

    ajout des variables de paramètre,test des fonctions async

--- index.js
+++ index.js
@@ -2,40 +2,52 @@ const pjlink = require('pjlink');
 const ObjectsToCsv = require('objects-to-csv');
 
 //const vpjson = require('./json/vplist_test.json');
-const vpjson = require('./json/vplist_cocteau.json');
+//const vpjson = require('./json/vplist_cocteau.json');
 //const vpjson = require('./json/vplist_dante.json');
-//const vpjson = require('./json/vp.json');
+const vpjson = require('./json/vp.json');
 
 //const vpip = "192.168.0.3"
 const vpport = 4352
 
 let vptableau = []
-
 let nbVP = 0
 var goCSV = 0
 
-const readline = require('node:readline');
+// Paramètre
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const createCSV = false
+const useReadline = false
+
+// Interface
+
+if(useReadline){
+    const readline = require('node:readline');
+    const { console } = require('node:inspector');
+
+    const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    });
+
+    rl.question(`Videoprojecteur 192.168.[---.---] OU entrée pour tous :`, input => {
+        if (input == ''){
+            rl.close();
+            return getAllProjo()
+        }else{
+            let vp = {}
+            vp['name'] = 'VP'
+            vp['ip'] = "192.168."+input
+            console.log(vp)
+            rl.close();
+            return getOneProjo(vp)
+        } 
+    });
+}else{
+    getAllProjo()
+}
 
 
-rl.question(`Videoprojecteur 192.168.[---.---] OU entrée pour tous :`, input => {
-    if (input == ''){
-        rl.close();
-        return getAllProjo()
-    }else{
-        let vp = {}
-        vp['name'] = 'VP'
-        vp['ip'] = "192.168."+input
-        console.log(vp)
-        rl.close();
-        return getOneProjo(vp)
-    } 
-});
-
+// Code
 
 function getAllProjo(){
     nbVP = vpjson.length
@@ -50,90 +62,82 @@ function getOneProjo(vp){
     return getDataProjo(vp) 
 }
 
-function getDataProjo(vp){
+async function getDataProjo(vp){
     const videoprojecteur = new pjlink(vp.ip, vpport)
 
+    let vpdata = {
+        'Nom du projecteur': vp.name,
+        'Adresse IP': vp.ip,
+        'Marque': '',
+        'Modèle': '',
+        'Référence': '',
+        'Lampe': '',
+        'Commentaire': ''
+    }
 
-    // let oui = videoprojecteur.getModel((err,model) => {
-    //     if(model == null){
-    //         console.log('yes')
-    //         return 'yes'
-    //     }else{
-    //         console.log('non')
-    //         return 'non'
+
+    console.log('Récupération des données pour', vp.name, '-', vp.ip, '...')
+
+    await ((var1)=>{
+        let var1
+        videoprojecteur.getModel(function (err,model) {
+            var1 = model
+        });
+        return var1
+    })
+
+    console.log(var1)
+    
+
+
+    
+
+    
+
+
+    // videoprojecteur.getPowerState(function (err, state) {
+
+    //     console.log('Récupération des données pour', vp.name, '-', vp.ip, '...')
+
+	// 	if(err){
+    //         vpdata['Commentaire'] = 'Pas de connexion'
+    //         console.log('Pas de connexion pour ' + vp.ip)
+    //         console.log(err)			
+	// 	}else{
+    //         videoprojecteur.getModel(async function (err,model) {
+    //             return vpdata['Modèle'] = model
+    //         });
+    
+    //         videoprojecteur.getInfo(function (err,info) {
+    //             return vpdata['Référence'] = info
+    //         });
+
+    //         videoprojecteur.getManufacturer(function (err,manufacturer) {
+    //             return vpdata['Marque'] = manufacturer
+    //         });
+    
+    //         videoprojecteur.getLamps(function (err,lamps) {
+    //             //vpdata['Lampe'] = lamps
+    //             return vpdata['Lampe'] = lamps?.[0]['hours']
+    //         }); 
+    //         console.log('OK')
     //     }
+
     // });
 
-    // console.log(oui)
-
-    videoprojecteur.getPowerState(function(err, state){
-
-        let vpdata = {
-            'Nom du projecteur': vp.name,
-            'Adresse IP': vp.ip,
-            'Marque': null,
-            'Modèle': null,
-            'Référence': null,
-            'Lampe': null,
-            'Commentaire': null
-        }
-
-        console.log('Récupération des données pour',vp.name,'-' ,vp.ip,'...')
-        
-
-		if(err){
-            vpdata['Commentaire'] = 'Pas de connexion'
-            console.log('Pas de connexion pour ' + vp.ip)
-            console.log(err)
-            //console.log(vptableau)
-            vptableau.push(vpdata)
-            goplus()
-			return
-		}else{
-            videoprojecteur.getModel(function (err,model) {
-                return vpdata['Modèle'] = model
-            });
+    vptableau.push(vpdata)
+    //console.log(vptableau)
+    goplus()
     
-            videoprojecteur.getInfo(function (err,info) {
-                return vpdata['Référence'] = info
-            });
-
-            videoprojecteur.getManufacturer(function (err,manufacturer) {
-                return vpdata['Marque'] = manufacturer
-            });
-    
-            videoprojecteur.getLamps(function (err,lamps) {
-                //vpdata['Lampe'] = lamps
-                return vpdata['Lampe'] = lamps?.[0]['hours']
-            });
-    
-            
-
-            // if(vpdata['Marque'] !== null && vpdata['Lampe'] !== null && vpdata['Référence'] !== null && vpdata['Modèle'] !== null){
-            //     console.log('OK Vérife')
-            //     console.log(vpdata)
-                
-            // }else{
-            //      console.log('non')
-            // }
-
-            
-            console.log('OK')
-            vptableau.push(vpdata)
-            console.log(vptableau)
-            goplus()
-            return 
-        }
-        
-	});
 }
 
 
-function goplus(){
+async function goplus(){
     goCSV = goCSV + 1
     console.log(goCSV,'/',nbVP)
     if (goCSV == nbVP){
-        getCSV()
+        createCSV ? await getCSV() : console.log('(CSV Off)') 
+        //end()
     }
     return
 }
@@ -146,7 +150,6 @@ async function getCSV(){
     await csv.toDisk('./tableauvp_'+time+'.csv');
     console.log('-----------------------------')
     console.log(await csv.toString());
-    end()
 };
 
 function getDate(){
